c0bb2814d0e0da872e64ba4029701a4d82b511e2
Jay Kanakiya <jaykanakiya42@gmail.com>
2019-01-19 16:38:01 +0000

    Delete application.py

--- application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-app = Flask(__name__)
-
-
-@app.route("/")
-def main():
-    return "Hello, world"
-    return render_template(os.path.join('templates', 'main.html'))
